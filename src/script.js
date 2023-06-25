43f62c136a85d64939814e89ce1c0a8c17310a06
AzureTiger <larrypotter0000@gmail.com>
2023-06-24 22:42:02 -0800

    bloom: no shader, no layer

--- src/script.js
+++ src/script.js
@@ -5,7 +5,6 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
-import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
@@ -53,7 +52,7 @@ const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
 
 // Lights
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
 // scene.add(ambientLight);
 
 // MODELVIEWER
@@ -81,22 +80,15 @@ scene.add(axes);
 let model_1, model_2, model_3;
 
 /**
- * ~~~~~~~~~~~~~~~~~~~~ Below is for Bloom ~~~~~~~~~~~~~~~~~~~~
+ * Bloom
  */
-const BLOOM_SCENE = 1; // Need help: how can I add more Bloom Layer???? :)
-
-const bloomLayer = new THREE.Layers();
-bloomLayer.set(BLOOM_SCENE);
 
 const params = {  // Need help for each parameter.. :)
-    threshold: 0,
+    threshold: 0.5,
     strength: 2,
-    radius: 0.3,
-    exposure: 0.5,
+    radius: 0.5,
+    exposure: 1,
 };
-
-const darkMaterial = new THREE.MeshBasicMaterial({ color: "black" });
-const materials = {};
 
 const renderScene = new RenderPass(scene, camera);
 
@@ -111,52 +103,9 @@ bloomPass.strength = params.strength;
 bloomPass.radius = params.radius;
 
 const bloomComposer = new EffectComposer(renderer);
-bloomComposer.renderToScreen = false;
+bloomComposer.renderToScreen = true;
 bloomComposer.addPass(renderScene);
 bloomComposer.addPass(bloomPass);
-
-const mixPass = new ShaderPass(
-    new THREE.ShaderMaterial({
-        uniforms: {
-            baseTexture: { value: null },
-            bloomTexture: { value: bloomComposer.renderTarget2.texture },
-        },
-        vertexShader: `
-            varying vec2 vUv;
-
-            void main() {
-
-                vUv = uv;
-
-                gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-            }
-        `,
-        fragmentShader: `
-            uniform sampler2D baseTexture;
-            uniform sampler2D bloomTexture;
-
-            varying vec2 vUv;
-
-            void main() {
-
-                gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
-
-            }
-        `,
-        defines: {},
-    }),
-    "baseTexture"
-);
-mixPass.needsSwap = true;
-
-const finalComposer = new EffectComposer(renderer);
-finalComposer.addPass(renderScene);
-finalComposer.addPass(mixPass);
-
-/**
- * ~~~~~~~~~~~~~~~~~~~~ Above is for Bloom ~~~~~~~~~~~~~~~~~~~~
- */
 
 /**
  * Models
@@ -172,23 +121,16 @@ gltfLoader.setDRACOLoader(dracoLoader);
 // Load main model
 gltfLoader.load("/models/1.glb", (gltf) => {
     model_1 = gltf.scene;
-    model_1.traverse((child) => {
-        if (child.name == "Sphere_3") {
-            console.log(3);
-            child.layers.toggle(BLOOM_SCENE); // ~~~~~~~~~~~~~~~~~~~~ This is for Bloom ~~~~~~~~~~~~~~~~~~~~
-        }
-    });
+    // model_1.traverse((child) => {
+    //     console.log(child);
+    //     if (child.name == "Sphere_2") {
+    //     }
+    // });
     scene.add(model_1);
 });
 gltfLoader.load("/models/2.glb", (gltf) => {
     model_2 = gltf.scene;
     model_2.position.y = 1;
-    model_2.traverse((child) => {
-        if (child.isMesh) { // bloom is only for mesh !!!
-            console.log(3);
-            child.layers.toggle(BLOOM_SCENE);
-        }
-    });
     scene.add(model_2);
 });
 gltfLoader.load("/models/3.glb", (gltf) => {
@@ -197,25 +139,6 @@ gltfLoader.load("/models/3.glb", (gltf) => {
     scene.add(model_3);
 });
 
-
-
-
-/**
- * ~~~~~~~~~~~~~~~~~~~~ Functions for Bloom ~~~~~~~~~~~~~~~~~~~~
- */
-function darkenNonBloomed(obj) {
-    if (obj.isMesh && bloomLayer.test(obj.layers) === false) {
-        materials[obj.uuid] = obj.material;
-        obj.material = darkMaterial;
-    }
-}
-
-function restoreMaterial(obj) {
-    if (materials[obj.uuid]) {
-        obj.material = materials[obj.uuid];
-        delete materials[obj.uuid];
-    }
-}
 
 /**
  * Action
@@ -239,13 +162,7 @@ const animate = () => {
     orbitControls.update();
 
     // Bloom
-    scene.traverse(darkenNonBloomed);
     bloomComposer.render();
-    scene.traverse(restoreMaterial);
-
-    finalComposer.render();
-
-    // Do not use renderer.render()!!!
 
     // Call animate again on the next frame
     window.requestAnimationFrame(animate);
